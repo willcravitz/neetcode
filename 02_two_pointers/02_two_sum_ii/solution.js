d7b348879b96af13fa5f20f9a2aafaa0ed0fcbf4
Will Cravitz <93291811+willcravitz@users.noreply.github.com>
2024-09-17 17:47:10 -0400

    prepped the next problem

---
+++ 02_two_pointers/02_two_sum_ii/solution.js
@@ -0,0 +1,23 @@
+/*
+Arrays & Hashing -> Two Sum II - Input Array is Sorted
+Neetcode 11 / 150
+Leetcode 167
+
+Javascript
+
+Comments:
+
+Time:
+Space:
+*/
+
+class Solution {
+	/**
+	 * @param {number[]} numbers
+	 * @param {number} target
+	 * @return {number[]}
+	 */
+	twoSum(numbers, target) {
+		return;
+	}
+}
---
+++ 02_two_pointers/02_two_sum_ii/solution.py
@@ -0,0 +1,17 @@
+"""
+Arrays & Hashing -> Two Sum II - Input Array is Sorted
+Neetcode 11 / 150
+Leetcode 167
+
+Python
+
+Comments:
+
+Time:
+Space:
+"""
+
+
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        pass
